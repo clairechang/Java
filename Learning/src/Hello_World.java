813ddbf4022e7d661032f7dc312c87896d990f75
rockst <rockst@gmail.com>
2016-09-08 17:48:25 +0800

    Hello World

---
+++ Learning/src/Hello_World.java
@@ -0,0 +1,14 @@
+import IO.Printer;
+
+// import java.lang.*; // 引用套件
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Printer My_Printer = new Printer();
+		My_Printer.serial_number = 5566;
+		My_Printer.Hello();
+	}
+	
+}
---
+++ Learning/src/IO/Printer.java
@@ -0,0 +1,13 @@
+package IO;
+
+public class Printer {
+	
+	public int serial_number;
+	
+	public void Hello() 
+	{
+		System.out.println("Hello Java!");
+		System.out.println("S/N: " + serial_number);
+		
+	}
+}
